3797a7cdebcc97673989a691a3a7b2e3f8572137
ANormalGameDev <96974897+ANormalGameDev@users.noreply.github.com>
2023-05-01 00:24:34 -0700

    Add files via upload

--- Deliv Reman/Character.gd
+++ Deliv Reman/Character.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
+@export var move_speed: float = 200.0
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("Movement.Right") - Input.get_action_strength("Movement.Left"),
 		Input.get_action_strength("Movement.Down") - Input.get_action_strength("Movement.Up")
 	)
+	
+	velocity = input_direction * move_speed
+	
+	_update_animation_state(input_direction)
+	move_and_slide()
+
+func _update_animation_state(input_direction: Vector2):
+	if input_direction != Vector2.ZERO:
+		state_machine.travel("Movement")
+		animation_tree.set("parameters/Movement/blend_position", input_direction)
+	else:
+		state_machine.travel("Idle")
